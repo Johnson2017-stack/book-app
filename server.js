c7d24aff7ed4346bd6ddc42aea4f8f1f6aeffd9c
Andrew Johnson <andrew.k.johnson228@gmail.com>
2023-06-14 18:01:55 -0700

    updated my server, added git ignore, added an array

--- server.js
+++ server.js
@@ -1,21 +1,56 @@
 const express = require('express');
 const ejs = require('ejs');
-const bodyParser = require('bosy-parser');
+const bodyParser = require('body-parser');
 
-const server = express()
+const server = express();
 
-server.use('views', './src/views');
-server.use("view engine", 'ejs');
+const books = [
+    {
+        id: 1,
+        title: "sourcers stone",
+        author:"jk rolling"
+    },
+    {
+        id: 2,
+        title: "chamber of secrets",
+        author: "jk rolling"
+    },
+    {
+        id: 3,
+        title: "too kill a mocking bird",
+        author: "me"
+    }
+]
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+server.set('views', './src/views');
+server.set('view engine', 'ejs');
 
-// parse application/json
-app.use(bodyParser.json())
+
+server.use(bodyParser.urlencoded({ extended: false }));
+
+
+server.use(bodyParser.json());
+
+server.get("/inventory", (res, req) => {
+    res.Render("inventory", {booksData: books})
+})
+
+server.delete("/books/:id", (res, req) => {
+    const id = req.params.id;
+    const bookIndex = books.find(book => book.id === id)
+
+    if (bookIndex === -1) {
+        res.send("404 - NOT FOUND")
+    }
+
+    books.splice(bookIndex, 1)
+    console.log(books)
+    res.send(`Your selection was deleted!!!`)
+})
 
 
 const PORT=3000;
 
 server.listen(PORT, () => {
-    console.log('listen on port ${PORT}')
-})
+    console.log(`listen on port ${PORT}`)
+});
